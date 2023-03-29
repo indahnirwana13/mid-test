a34bd5830b72c293352ad7e4dc14d994f318fa71
indahnirwana13 <127530332+indahnirwana13@users.noreply.github.com>
2023-03-28 23:40:07 -0700

    Update TestErr20012.java

--- soal no 1/TestErr20012.java
+++ soal no 1/TestErr20012.java
@@ -9,14 +9,17 @@ public class TestErr20012 {
         for (int i = 0; i < 10; i++) {
             tot += i;
             if (i < j) {
-                System.out.println(i);
+            System.out.println(i);
             } else {
-                System.out.println(j);
+            System.out.println(j);
             }
         }
-        do {
+        do   {
+            
             j++;
+        
         } while (j < 10);
+        
         while (j < 10) {
             j++;
         }
